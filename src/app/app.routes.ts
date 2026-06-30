dccc621755596b430b85c94c1a6b5943b494c2c4
Jansaurus00 <jangabriel.soriano@gmail.com>
2026-06-30 10:31:33 +0800

    Created frontend design for Home page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,14 @@
 import { Routes } from '@angular/router';
 import { Hunters } from './pages/hunters/hunters';
 import { Home } from './pages/home/home';
+import { Dashboard } from './pages/dashboard/dashboard';
+import { Monsters } from './pages/monsters/monsters';
+import { Weapons } from './pages/weapons/weapons';
 
 export const routes: Routes = [
     {path:'', component: Home },
+    {path:'dashboard', component: Dashboard},
     {path:'hunters', component: Hunters},
+    {path:'monsters', component: Monsters},
+    {path:'weapons', component: Weapons}
 ];
